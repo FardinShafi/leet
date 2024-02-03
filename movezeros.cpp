4343c3d33c5b4796ab467ac40c2d582e62563f4d
NITRO\fardin <fardin467@gmail.com>
2024-02-04 00:37:54 +0600

    movezeroes

---
+++ movezeros.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int nonZeroIndex = 0;
+
+        // Move all non-zero elements to the front
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                nums[nonZeroIndex++] = nums[i];
+            }
+        }
+
+        // Fill the remaining elements with zeros
+        for (int i = nonZeroIndex; i < nums.size(); i++) {
+            nums[i] = 0;
+        }
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<int> myVector = {1, 2, 0, 3, 4, 0, 5, 6};
+
+    cout << "Original Vector: ";
+    for (int i =0; i<myVector.size();i++) {
+        cout << myVector[i] << " ";
+    }
+    cout << endl;
+
+    solution.moveZeroes(myVector);
+
+    cout << "Modified Vector: ";
+    for (int i =0; i<myVector.size();i++) {
+        cout << myVector[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
